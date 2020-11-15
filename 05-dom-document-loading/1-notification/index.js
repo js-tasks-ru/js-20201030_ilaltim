afa7205ab9e3a00f735e556869ac9574f201414c
Ilya Edelshtein <ilaltim@gmail.com>
2020-11-15 19:43:23 +0200

    notificator v3.0 final no bugs

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,15 +1,29 @@
 export default class NotificationMessage {
+ static elem = null;
 
-  static isShown = false;
+ constructor(message = 'some msg',
+   {duration = 1000, type = 'success'} = {}) {
+   this.message = message;
+   this.duration = duration;
+   this.type = type;
+  this.show();
 
-  constructor(message = 'some msg',
-    {duration = 1000, type = 'success'} = {}) {
-    this.message = message;
-    this.duration = duration;
-    this.type = type;
+ }
 
-    this.element = document.createElement('div');
-    this.element.innerHTML = `<div class="notification ${this.type} note" style="--value:${this.duration / 1000}s">
+
+
+ show(...args) {
+
+   if (args.length) {
+     args[0].insertAdjacentHTML('afterbegin', this.element.outerHTML);
+     return;
+   }
+
+
+   if (!NotificationMessage.elem) {
+     const element = document.createElement('div');
+     element.innerHTML = `
+<div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
     <div class="timer"></div>
     <div class="inner-wrapper">
       <div class="notification-header">${this.type}</div>
@@ -18,69 +32,41 @@ export default class NotificationMessage {
       </div>
     </div>
   </div>`;
-    this.element = this.element.firstElementChild;
-    //без таймаута не порходят тесты.
-    setTimeout(() => this.show());
-  }
+     this.element = element.firstElementChild;
+     NotificationMessage.elem = this.element;
+     document.body.append(NotificationMessage.elem);
 
 
-  show(...args) {
-    if (args.length) {
-      args[0].insertAdjacentHTML('afterbegin', this.element.outerHTML);
-      return;
-    }
-    /*
-   если фолс то добавляем элемент, ставим таймаут на удаление(ПОСЛЕ УДАЛЕНИЯ ТАЙМАУТ МЕНЯЕТ
-    СЩСТОЯНИЕ НА ФОЛС)
-    и ПОСЛЕ ТАЙМАУТА =>
-   меняем состояние на тру
-   ретурн
-   */
-    if (!NotificationMessage.isShown) {
-      document.body.append(this.element);
+     setTimeout(()=> {
+       this.remove();
+     }
+     , this.duration);
+     return ;
+   }
+   else if(NotificationMessage.elem){
+     this.element = NotificationMessage.elem;
+     this.remove();
+     NotificationMessage.elem = null;
 
-      setTimeout(() => {
-        this.remove();
-        NotificationMessage.isShown = false;
-      }, this.duration);
-
-      NotificationMessage.isShown = true;
-      return;
-    }
-
-    /*
-  если тру
-  то ищем элемент по note
-  удаляем элемент
-  ставим новый элемент
-  ставим на него таймер(ПОСЛЕ УДАЛЕНИЯ ТАЙМЕР МЕНЯЕТ СТЕЙТ НА ФОЛС) на удаление
-  ретурн
-   */
-
-    if (NotificationMessage.isShown) {
-      let elem = document.body.querySelectorAll('.note');
-      elem.forEach(el=> el.remove())
-      document.body.append(this.element);
+     this.show();
+   }
 
 
-      setTimeout(() => {
-        this.remove();
-        NotificationMessage.isShown = false;
-      }, this.duration);
-
-    }
 
 
-  }
 
 
-  remove() {
-    this.element.remove();
-  }
 
-  destroy() {
-    this.remove();
-    //this.element = null;
-  }
+
+
+ }
+ remove() {
+   this.element.remove();
+ }
+
+ destroy() {
+   this.remove();
+   //this.element = null;
+ }
 
 }
